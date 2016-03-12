8a188c3f33afbee7753f5514d02bdd5cd6da9a6e
paulserafim <serafim.paul@gmail.com>
2016-03-12 20:20:32 +0000

    Refactor code from content.js to popup.js. Button changing from Join to Leave and vice versa.

---
+++ extension/js/popup.js
@@ -0,0 +1,22 @@
+/**
+ * Created by arghasarkar on 12/03/2016.
+ */
+
+function hideButton() {
+	$("#leaveButton").hide();
+	$("#joinButton").click(function() {
+	$("#leaveButton").show();
+	$("#joinButton").hide();
+	document.getElementById("nameField").disabled = true;
+	document.getElementById("groupField").disabled = true;
+
+});
+$("#leaveButton").click(function() {
+	$("#joinButton").show();
+	$("#leaveButton").hide();
+	document.getElementById("nameField").disabled = false;
+	document.getElementById("groupField").disabled = false;
+
+});
+}
+
