1b1830cc5ad50fe52525c17bfebe8538735243fa
paulserafim <serafim.paul@gmail.com>
2016-03-12 19:32:28 +0000

    JavaScript

--- extension/js/content.js
+++ extension/js/content.js
@@ -1,3 +1,18 @@
 /**
  * Created by arghasarkar on 12/03/2016.
  */
+$("#leaveButton").hide();
+$("#joinButton").click(function() {
+	$("#leaveButton").show();
+	$("#joinButton").hide();
+	document.getElementById("nameField").disabled = true;
+	document.getElementById("groupField").disabled = true;
+
+});
+$("#leaveButton").click(function() {
+	$("#joinButton").show();
+	$("#leaveButton").hide();
+	document.getElementById("nameField").disabled = false;
+	document.getElementById("groupField").disabled = false;
+
+});
