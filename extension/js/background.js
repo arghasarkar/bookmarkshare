ed7b58e50eb79d25183c4797061bb6c42cd0ccb4
Argha Sarkar <argha.sarkar1994@gmail.com>
2016-03-12 17:13:32 +0000

    Added a JavaScript method in background.js to send POST requests to a backend server.

--- extension/js/background.js
+++ extension/js/background.js
@@ -7,6 +7,31 @@ chrome.contextMenus.create({
     'onclick': onClickShareLinkHandler
 });
 
+function sendPostRequest(path, params, method) {
+    /*
+     * Will send a POST request to the backend server.
+     */
+    method = method || "post";
+
+    var form = document.createElement("form");
+    form.setAttribute("method", method);
+    form.setAttribute("action", path);
+
+    for(var key in params) {
+        if(params.hasOwnProperty(key)) {
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
+
+            form.appendChild(hiddenField);
+        }
+    }
+
+    document.body.appendChild(form);
+    form.submit();
+}
+
 function onClickShareLinkHandler(info) {
     /**
      *  Code will go here for sending a request to the backend server containing the URL.
