ea7783c5e1d1458a0db2cf46e1f20e8d0e1dcd66
Argha Sarkar <argha@arghasarkar.co.uk>
2016-10-02 01:23:42 +0100

    feat(website): Create a new public facing website
    - Use an online template to create a website with information about the
      chrome extension.
    
    - Modify the Route.php file to redirect the root directory to the index
      of this new website.
    
    [QA]
    Has tests: No
    Reviewed by: NIL

--- server/app/Http/routes.php
+++ server/app/Http/routes.php
@@ -12,7 +12,10 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+Route::get('/*', function () {
+    return view('index');
 });
 
 Route::get('/api/group/join/{group_name}', 'GroupsController@join');
---
+++ server/resources/views/index.blade.php
@@ -0,0 +1,242 @@
+<!DOCTYPE HTML>
+<!--
+	Hyperspace by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Bookmark Share</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]>
+
+		<script src="/assets/js/ie/html5shiv.js"></script><![endif]-->
+
+		<link rel="stylesheet" href="/assets/css/main.css" />
+		<!--[if lte IE 9]>
+		<link rel="stylesheet" href="/assets/css/ie9.css"/><![endif]-->
+		<!--[if lte IE 8]>
+		<link rel="stylesheet" href="/assets/css/ie8.css"/><![endif]-->
+	</head>
+	<body>
+
+		<!-- Sidebar -->
+			<section id="sidebar">
+				<div class="inner">
+					<nav>
+						<ul>
+							<li><a href="#intro">Welcome</a></li>
+							<li><a href="#one">What am I?</a></li>
+							<li><a href="#two">Features</a></li>
+							<li><a href="#three">Contribution</a></li>
+						</ul>
+					</nav>
+				</div>
+			</section>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Intro -->
+					<section id="intro" class="wrapper style1 fullscreen fade-up">
+						<div class="inner">
+							<h1><a href="https://chrome.google.com/webstore/detail/bookmarks-share/ajokpilhjfcpningdgndcpekaaakeegh/">Bookmark Share</a></h1>
+							<p>The best way to share Bookmarks with your friends and colleagues!</p>
+							<p>
+								A web application and a chrome browser extension which allows Bookmarks to be shared between you, your friends and colleagues
+								in real time at the click of a button.
+							</p>
+							<ul class="actions">
+								<li>
+									<a href="https://chrome.google.com/webstore/detail/bookmarks-share/ajokpilhjfcpningdgndcpekaaakeegh/"
+									   target="_blank" class="button chrome-web-store-link" id="chrome-web-store-link">
+										<!--<img id="chrome-icon-link" src="http://icons.iconarchive.com/icons/google/chrome/24/Google-Chrome-icon.png" alt="Google Chrome Browser Icon">-->
+										&nbsp; &nbsp; Install Chrome Extension
+									</a>
+								</li>
+								<li><a href="#one" class="button scrolly">Learn more</a></li>
+
+							</ul>
+						</div>
+					</section>
+
+				<!-- One -->
+					<section id="one" class="wrapper style2 spotlights">
+						<section>
+							<a href="#" class="image"><img src="images/chrome-logo-512.png" alt="" data-position="center center" /></a>
+							<!-- Source: https://cdn0.iconfinder.com/data/icons/jfk/512/chrome-512.png -->
+							<div class="content">
+								<div class="inner">
+									<h2>Google chrome extension</h2>
+									<p>
+										<a href="https://chrome.google.com/webstore/detail/bookmarks-share/ajokpilhjfcpningdgndcpekaaakeegh/"
+										   alt="Link to Bookmarkshare" target="_blank">Bookmark Share </a> will allow
+										you to share your links without having to leave the page you are on.
+									</p>
+									<ul class="actions">
+										<li>
+											<a href="https://chrome.google.com/webstore/detail/bookmarks-share/ajokpilhjfcpningdgndcpekaaakeegh/"
+											   target="_blank"  class="button chrome-web-store-link">
+												&nbsp;&nbsp;Install now
+											</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</section>
+						<section>
+							<a href="#" class="image"><img src="images/bms-what-am-i.png" alt="" data-position="top center" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>Web application</h2>
+									<p>
+										The web application allows non-Chrome users to be able to share links and receive
+										links with other users without the need to install any software.
+									</p>
+									<p>
+										This is primarily aimed at Safari and Firefox users.
+									</p>
+									<ul class="actions">
+										<li><a href="" class="button" onclick="alert('This feature will be implemented shortly!')">Learn more</a></li>
+									</ul>
+								</div>
+							</div>
+						</section>
+
+				<!-- Two -->
+					<section id="two" class="wrapper style3 fade-up">
+						<div class="inner">
+							<h2>Features</h2>
+
+							<div class="features">
+								<section>
+									<span class="icon major fa-code"></span>
+									<h3>Open source @
+										<a href="https://github.com/arghasarkar/bookmarkshare" target="_blank">Github</a>
+									</h3>
+									<p>
+										This project was developed at BrumHack and is completely open source. Feel free
+										to contribute. Click <a href="https://github.com/arghasarkar/bookmarkshare"
+																target="_blank"> here</a> for the repository.
+									</p>
+								</section>
+								<section>
+									<span class="icon major fa-diamond"></span>
+									<h3>No sign-up required</h3>
+									<p>
+										No need to create an account. Just join or create a group with a memorable name.
+									</p>
+								</section>
+								<section>
+									<span class="icon major fa-cog"></span>
+									<h3>Group sharing</h3>
+									<p>
+										Create a group and invite your friends to share links. Otherwise, join a group
+										your friend has created just by typing in the group name.
+									</p>
+									<p>
+										Switch between multiple groups with ease.
+									</p>
+								</section>
+								<section>
+									<span class="icon major fa-desktop"></span>
+									<h3>Multi-platform</h3>
+									<p>
+										Available from your PC, Mac and your phone.
+									</p>
+									<p>
+										Available both as a chrome extension and web application (no installation required).
+									</p>
+								</section>
+								<section>
+									<span class="icon major fa-chain"></span>
+									<h3>Real-time sharing</h3>
+									<p>
+										Real-time bookmark sharing enabled by Pusher.
+									</p>
+								</section>
+								<section>
+									<span class="icon major fa-diamond"></span>
+									<h3>One-click share</h3>
+									<p>
+										Share links with your group with two simple clicks:
+										<ol>
+											<li>Right-click to show context menu</li>
+											<li>Click on "Share this URL"</li>
+										</ol>
+									</p>
+								</section>
+							</div>
+						</div>
+					</section>
+
+				<!-- Three -->
+					<section id="three" class="wrapper style1 fade-up">
+						<div class="inner">
+							<h2>Open source contribution</h2>
+							<p>
+								As this project is <b>FREE</b> and <b><a href="https://github.com/arghasarkar/bookmarkshare"
+																		 target="_blank">open source</a></b>,
+								contributions towards the development of the project is appreciated. Please checkout the
+								<a href="https://github.com/arghasarkar/bookmarkshare" target="_blank">Github</a>
+								repository and contribute towards the development by working towards new feature
+								requests and fixing bugs.
+							</p>
+							<div class="split style1">
+								<section>
+									<form method="post" action="#">
+										<div class="field half first">
+											<label for="name">Name</label>
+											<input type="text" name="name" id="name" onclick="disabledFormAlert()" disabled/>
+										</div>
+										<div class="field half">
+											<label for="email">Email</label>
+											<input type="text" name="email" id="email" onclick="disabledFormAlert()" disabled/>
+										</div>
+										<div class="field">
+											<label for="message">Message</label>
+											<textarea name="message" id="message" rows="5" onclick="disabledFormAlert()" disabled></textarea>
+										</div>
+										<ul class="actions">
+											<li><a href="" class="button submit" onclick="disabledFormAlert()">Send Message</a></li>
+										</ul>
+									</form>
+								</section>
+								<section>
+									<ul class="contact">
+
+										<li>
+											<h3>Email</h3>
+											<a href="mailto:argha.sarkar1994@gmail.com?subject=Bookmark Share" id="contactEmailLink">argha.sarkar1994@gmail.com</a>
+										</li>
+
+									</ul>
+								</section>
+							</div>
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Footer -->
+			<footer id="footer" class="wrapper style1-alt">
+				<div class="inner">
+					<ul class="menu">
+						<li>BookmarkShare &copy; Argha Sarkar 2016. All rights reserved.</li><li>Design and template from: <a href="http://html5up.net">HTML5 UP</a></li>
+					</ul>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="/assets/js/jquery.min.js"></script>
+			<script src="/assets/js/jquery.scrollex.min.js"></script>
+			<script src="/assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="/assets/js/util.js"></script>
+			<!--[if lte IE 8]>
+			<script src="/assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="/assets/js/main.js"></script>
+
+	</body>
+</html>
