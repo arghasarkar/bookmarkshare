b0b29aa5786474f1d9fd718f000feb45f2bb20d8
Argha Sarkar <argha@arghasarkar.co.uk>
2016-10-16 06:01:14 +0000

    feat(HJ): Got the two models and migrations to work
    - Models and migrations of HUsers and Skills to work

---
+++ server/app/HUser.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class HUser extends Model
+{
+
+    protected $fillable = [
+        'name',
+        'mob',
+        'email'
+    ];
+    
+}
--- server/database/seeds/DatabaseSeeder.php
+++ server/database/seeds/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Database\Seeder;
 
+use \App\HUser;
+use \App\Skill;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -11,6 +14,76 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserTableSeeder::class);
+        $this->call('HUserTableSeeder');
+
+        $this->call('SkillTableSeeder');
     }
+}
+
+class HUserTableSeeder extends Seeder
+{
+
+    public function run() {
+        DB::table('h_users')->delete();
+        
+        HUser::create([
+            "name" => "Argha",
+            "email" => "argha@arghasarkar.co.uk",
+            "mob" => "+447554164303"
+        ]);
+
+        HUser::create([
+            "name" => "Adam",
+            "email" => "adam@example.com",
+            "mob" => ""
+        ]);
+
+        HUser::create([
+            "name" => "Gisi",
+            "email" => "gisi@example.com",
+            "mob" => ""
+        ]);
+
+        HUser::create([
+            "name" => "Fernando",
+            "email" => "fernando@example.com",
+            "mob" => ""
+        ]);
+    }
+
+}
+
+class SkillTableSeeder extends Seeder {
+
+    public function run() {
+
+        DB::table('skills')->delete();
+
+        Skill::create([
+            "name" => "Java",
+            "description" => "Java programming language."
+        ]);
+
+        Skill::create([
+            "name" => "Front-end web developer",
+            "description" => "Front end web developer"
+        ]);
+
+        Skill::create([
+            "name" => "OpenCV",
+            "description" => "Computer vision library primarily in C++"
+        ]);
+
+        Skill::create([
+            "name" => "ReactJS",
+            "description" => "Javascript library"
+        ]);
+
+        Skill::create([
+            "name" => "Python",
+            "description" => "Python programming language"
+        ]);
+
+    }
+
 }
