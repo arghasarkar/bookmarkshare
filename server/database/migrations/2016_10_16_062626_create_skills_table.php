14322be6fb7c81d72f1762af4cb1ee2d6f53babd
Argha Sarkar <argha.sarkar1994@gmail.com>
2016-10-16 08:47:45 +0200

    Made the migration for the skill table

--- server/database/migrations/2016_10_16_062626_create_skills_table.php
+++ server/database/migrations/2016_10_16_062626_create_skills_table.php
@@ -14,6 +14,8 @@ class CreateSkillsTable extends Migration
     {
         Schema::create('skills', function (Blueprint $table) {
             $table->increments('id');
+            $table->string("name");
+            $table->string("description");
             $table->timestamps();
         });
     }
