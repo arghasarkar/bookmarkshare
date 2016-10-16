9251f045c90c0ef02a31f148f9ffb7d274b8911a
Argha Sarkar <argha.sarkar1994@gmail.com>
2016-10-16 09:40:52 +0200

    Changed the main run function

--- server/database/seeds/DatabaseSeeder.php
+++ server/database/seeds/DatabaseSeeder.php
@@ -14,7 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(HUserTableSeeder::class);
+        $this->call('HUserTableSeeder');
+
+        $this->call('SkillTableSeeder');
     }
 }
 
