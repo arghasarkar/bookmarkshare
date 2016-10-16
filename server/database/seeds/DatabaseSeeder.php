271318a0d596a51e70f566c46ebde7c409a05067
Argha Sarkar <argha.sarkar1994@gmail.com>
2016-10-16 09:11:52 +0200

    WIP: Added HUser seeder

--- server/database/seeds/DatabaseSeeder.php
+++ server/database/seeds/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Database\Seeder;
 
+use \App\HUser;
+use \App\Skill;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -11,6 +14,38 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserTableSeeder::class);
+        $this->call(HUserTableSeeder::class);
     }
+}
+
+class HUserTableSeeder extends Seeder
+{
+    public function run() {
+        DB::table('h_users')->delete();
+
+        HUser::create([
+            "name" => "Argha",
+            "email" => "argha@arghasarkar.co.uk",
+            "mob" => "+447554164303"
+        ]);
+
+        HUser::create([
+            "name" => "Adam",
+            "email" => "adam@example.com",
+            "mob" => ""
+        ]);
+
+        HUser::create([
+            "name" => "Gisi",
+            "email" => "gisi@example.com",
+            "mob" => ""
+        ]);
+
+        HUser::create([
+            "name" => "Fernando",
+            "email" => "fernando@example.com",
+            "mob" => ""
+        ]);
+    }
+    
 }
